61c3919f498d01a82cdaa4e9dc5598435f738500
Viktor Belous <vyntyk@yandex.ru>
2022-08-09 19:11:35 +0300

    Задача 13 решена Logarifm

--- Papka2/Program.cs
+++ Papka2/Program.cs
@@ -6,16 +6,23 @@
 32679 -> 6
 */
 
-
 Console.Write("Please enter your number: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+int position = 3;
 
-string anyNumberText = Console.ReadLine()!;
-
-if (anyNumberText.Length > 2)
+if (anyNumber < 100)
 {
-    Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+    Console.WriteLine($"{anyNumber} -> третьей цифры нет");
 }
 else
 {
-    Console.WriteLine("-> третьей цифры нет");
+    Console.WriteLine($"{position} цифра: " + FindNumber(anyNumber, position));
+}
+
+int FindNumber(int anyNumber, int position)
+{
+    int digitCount = (int)Math.Log10(anyNumber) + 1;
+
+    var pow = (int)Math.Pow(10, digitCount - position);
+    return (anyNumber / pow) % 10;
 }
