0a2330338aaf8ef487a177f5cf4bb5aa29d9caf8
Viktor Belous <vyntyk@yandex.ru>
2022-08-09 18:04:10 +0300

    Задача 13 решена через string

---
+++ Papka2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 13: Напишите программу, которая выводит 
+третью цифру заданного числа или сообщает,
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+
+Console.Write("Please enter your number: ");
+
+string anyNumberText = Console.ReadLine()!;
+
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else
+{
+    Console.WriteLine("-> третьей цифры нет");
+}
---
+++ Papka3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 15: Напишите программу, которая принимает
+ на вход цифру, обозначающую день недели,
+  и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1 && number <= 7)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Да, выходной");
+    }
+    else
+    {
+        Console.WriteLine("Нет, рабочий день");
+    }
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7 включительно");
+}
